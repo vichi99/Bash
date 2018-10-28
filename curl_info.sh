3ec0bfef750831b8372e9b6e53f421bbb567b095
jan <john.vicha@seznam.cz>
2018-10-28 10:30:40 +0100

    -

--- curl_info.sh
+++ curl_info.sh
@@ -3,7 +3,7 @@
 #Code through private key provide ssh login to PC. Control if exist user and if not, make
 #them and assing ssh public key from http address. Then control if in authorized_keys is exists users
 #public key from http, if not, add them.
-#does not work on 100%ss
+#does not work on 100%
 ######
 
 
